e4e7485ee89f25dfa8f08b4ef0d7c6fb2b7ec76a
Sara Elizabeth Anderson <Sara@adsl-172-8-25-251.dsl.lsan03.sbcglobal.net>
2021-06-18 16:46:37 -0700

    Started working on Local storage setItem.

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
 const m = moment();
 $('#currentDay').text(m.format("dddd, MMM Do YYYY, h:mm:ss a"));
 
+function getTime () {
+    var currentTime = moment().hour();
 
+
+}
+
+
+/*Event listener for Save Button
+$(".saveBtn").on("click", function() {
+
+}
+*/
+
+// Local Storage
+localStorage.setItem(time, text);
