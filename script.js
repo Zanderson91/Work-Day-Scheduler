ba0ccc8524b122d137d8e91acd9a402eb3b4929e
Sara Elizabeth Anderson <Sara@Saras-Mac.local>
2021-06-21 18:37:00 -0700

    Testing storage to local storage

--- script.js
+++ script.js
@@ -3,38 +3,9 @@ const m = moment();
 $('#currentDay').text(m.format("dddd, MMM Do YYYY, h:mm:ss a"));
 
 
-function timeBlockEvent() {
-    let currentTime = m;
-
-
-
-
 //need conditionals for time blocks
-if (timeBlock < currentTime) {
-    $(this).addClass("past");
-    $(this).removeClass("present");
-    $(this).removeClass("future");
-}else if (timeBlock === currentTime) { 
-    $(this).addClass("present");
-    $(this).removeClass("past");
-    $(this).removeClass("future");
-}else { $(this).addClass("future");
-$(this).removeClass("present");
-$(this).removeClass("past");
-}
 
 
-
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-$("#hour2 .description").val(localStorage.getItem("hour2"));
-$("#hour3 .description").val(localStorage.getItem("hour3"));
-$("#hour4 .description").val(localStorage.getItem("hour4"));
-$("#hour5 .description").val(localStorage.getItem("hour5"));
 
 
 
@@ -60,6 +31,31 @@ let time = $(this).parent().attr("id");
 // Local Storage
 localStorage.setItem(time, notes);
 
-})
-})
+
+    // loop over div time blocks
+$(".time-block").each(function () {
+    let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+    let currentTime = moment().hour();
+
+if (timeBlock < currentTime) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    }else if (timeBlock === currentTime) { 
+    $(this).addClass("present");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    }else { $(this).addClass("future");
+    $(this).removeClass("present");
+    $(this).removeClass("past");
 }
+              
+
+})
+})
+})
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+
+
+
