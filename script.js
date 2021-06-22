be982b3136a38ea1c3d438c6dd81f6aabfb12c05
Sara Elizabeth Anderson <Sara@Saras-Mac.local>
2021-06-21 18:15:56 -0700

    Added i:hover class to button class in HTML.

--- script.js
+++ script.js
@@ -25,7 +25,16 @@ $(this).removeClass("past");
 
 
 
-
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
 
 
 
@@ -50,5 +59,7 @@ let time = $(this).parent().attr("id");
 
 // Local Storage
 localStorage.setItem(time, notes);
-})})})
+
+})
+})
 }
