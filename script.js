663491a4da57987a281eaba853a20fbb25e12891
Sara Elizabeth Anderson <Sara@Saras-Mac.local>
2021-06-21 18:38:46 -0700

    Saving local storage for all time blocks 8-5.

--- script.js
+++ script.js
@@ -16,12 +16,6 @@ $('#currentDay').text(m.format("dddd, MMM Do YYYY, h:mm:ss a"));
 
 
 
-//need indication for classes added in CSS
-
-
-//need value from local storage
-
-
 $(document).ready(function() {
 //Event listener for Save Button
 $(".saveBtn").on("click", function() {
@@ -56,6 +50,15 @@ if (timeBlock < currentTime) {
 })
 
 $("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
 
 
 
